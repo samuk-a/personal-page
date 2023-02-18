4eb6decfc3917c310373b449ab8e07397b86d2d2
Samuel <samuel.santiago@ufu.br>
2023-02-18 17:45:13 -0300

    prettier files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
-const routerBase = process.env.DEPLOY_ENV === "GITHUB_PAGES" ? { router: { base: "/" } } : {};
+const routerBase =
+  process.env.DEPLOY_ENV === 'GITHUB_PAGES' ? { router: { base: '/' } } : {}
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
