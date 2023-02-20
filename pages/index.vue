b8abdbcd8038964628f66bbf04a1597f257d7f90
Samuel <samuel.santiago@ufu.br>
2023-02-20 10:00:50 -0300

    fix(index.vue): change cols

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
   <v-row align="center">
-    <v-col sm="12" md="8" lg="7">
+    <v-col cols="12" sm="8" md="7" :order="order">
       <h1 style="text-align: center">Samuel Santiago</h1>
     </v-col>
-    <v-col sm="12" md="4" lg="3">
+    <v-col cols="12" sm="4" md="3">
       <object type="image/svg+xml" data="developer.svg"></object>
     </v-col>
   </v-row>
 </template>
+
+<script lang="ts">
+export default {
+  computed: {
+    order() {
+      const { smAndUp } = this.$vuetify.display
+      return smAndUp ? 'first' : 'last'
+    },
+  },
+}
+</script>
