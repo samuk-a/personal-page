ee29ce19525710ae2a58617a8b90a747677c2ade
Samuel <samuel.santiago@ufu.br>
2023-02-20 00:16:36 -0300

    feat: add svg to home

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Home</h1>
+  <v-row align="center">
+    <v-col sm="12" md="8" lg="7">
+      <h1 style="text-align: center">Samuel Santiago</h1>
+    </v-col>
+    <v-col sm="12" md="4" lg="3">
+      <object type="image/svg+xml" data="developer.svg"></object>
+    </v-col>
+  </v-row>
 </template>
